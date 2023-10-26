c9cd5b57cddbbbcb24a07b577b796a9227f41217
Abdul Rehman Sohail <abdulrehmansohail786@gmail.com>
2023-10-26 15:45:57 +0500

    Base Template for Job listing

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+JOBS = [
+    {
+        'id': '1',
+        'title': 'Python Engineer',
+        'location': 'Lahore, PK',
+        'salary': '400,000',
+    },
+    {
+        'id': '2',
+        'title': 'QA Engineer',
+        'location': 'ISL, PK',
+        'salary': '300,000',
+    },
+    {
+        'id': '1',
+        'title': 'Dev Ops',
+        'location': 'Karachi, PK',
+        'salary': '600,000',
+    },
+]
 
 
 @app.route("/")
 def home():
-    return "<h1>Welcome to Home Page</h1>"
+    return render_template("home.html", jobs=JOBS)
+
+
+@app.route("/api/jobs")
+def jobs_list():
+    return jsonify(JOBS)
 
 
 if __name__ == "__main__":
